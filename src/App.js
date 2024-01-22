29a760f56468b45955178432a1704ae3c880cc9b
Martin Elizbaryan <martin.elizbaryan@gmail.com>
2024-01-22 21:34:53 +0400

    lesson 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+
+const { log } = console;
 
 function App() {
-  return <div>Hello</div>;
+  const [count, setCount] = useState(0); // return [variable, setVariable]
+  log(count);
+
+  const addCount = () => {
+    setCount(count + 1);
+    // setCount((prevSate) => {
+    //   return prevSate + 1;
+    // });
+
+    // log(count);
+  };
+
+  return (
+    <div>
+      <button onClick={addCount}>+</button>
+      <input value="5" />
+      <p>{count}</p>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// const { log } = console;
+
+// const arr = [1, 2, 3, 4];
+// // const firstItem = arr[0];
+// // const secItem = arr[1];
+// // const restArr = [arr[2], arr[3]];
+// const [firstItem, secItem, ...restArr] = arr;
+
+// log({ firstItem, secItem, restArr });
+
+// const user = { name: "Martin", surname: "Elizbaryan" };
+// // const name = user.name;
+// // const surname = user.surname;
+
+// const { name: auwdghkuagw = "Martinnnnn", surname } = user;
+
+// log({ auwdghkuagw, surname });
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
